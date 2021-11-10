5ffc0df98df669de756d4d0c543f3de8319fda9a
Gilbert Rehling <gilbert.rehling@gmail.com>
2021-11-10 20:47:55 +1100

    testing for correct docker build process

--- pmasetup.sh
+++ pmasetup.sh
@@ -6,12 +6,16 @@ pmasetup() {
     COMPOSE_FILE="./docker/docker-compose.yml"
     DOCKER_WEB="docker_php-mongo-web_1"
     DOCKER_DB="docker_php-mongo-db_1"
+    SOURCE="$PMA_DIR/docker/build/docker/pma-mongo-web/config/env.example"
+    TARGET="$PMA_DIR/.env"
 
     COLOR_RED="$(tput setaf 1)"
     COLOR_NONE="$(tput sgr0)"
     COLOR_BLUE="$(tput setaf 6)"
 
-    echo "${COLOR_BLUE}Working DIR: $PMA_DIR"
+    echo "${COLOR_BLUE}Working DIR : $PMA_DIR"
+    echo "${COLOR_BLUE}source : $SOURCE"
+    echo "${COLOR_BLUE}target: $TARGET"
 
     COMMAND=$1
 
@@ -61,7 +65,7 @@ pmasetup() {
         # check env file exists
         if [ ! -e .env ]; then
             echo "${COLOR_RED} env file missing - copying example"
-            cp --verbose ./docker/build/docker/pma-mongo-admin/config/env.example .env
+            cp --verbose SOURCE .env
         fi
         docker exec -it $DOCKER_WEB /bin/bash -c "cd /usr/share/phpMongoAdmin && composer $*"
         ;;
@@ -71,7 +75,7 @@ pmasetup() {
         # check env file exists
         if [ ! -e .env ]; then
             echo "${COLOR_RED} env file missing - copying example"
-            cp --verbose "$PMA_DIR/docker/build/docker/pma-mongo-admin/config/env.example" "$PMA_DIR/.env"
+            cp --verbose SOURCE TARGET
         fi;\
         winpty docker exec -it $DOCKER_WEB bash -c "cd /usr/share/phpMongoAdmin && composer $*"
         ;;
